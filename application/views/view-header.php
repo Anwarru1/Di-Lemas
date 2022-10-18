96a15fa12aee3b67831c936f843187c41b97b472
Anwarru1 <anwaruromadon@gmail.com>
2022-10-18 10:59:43 +0700

    first commit

---
+++ application/views/view-header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Di-Lemas</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/datasiswa.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>Di-Lemas</h1>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
